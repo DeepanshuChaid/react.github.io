00be4cb2fdf067f713ae406de7397b7c84b7f9a6
DeepanshuChaid <bantar117@gmail.com>
2025-05-22 10:16:26 +0000

    add react file

---
+++ react/src/App.jsx
@@ -0,0 +1,9 @@
+import Card from './Card.jsx'
+
+function App() {
+  <>
+    <Card/>
+  </>
+}
+
+export default App
---
+++ react/src/Card.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+function Card() {
+    return (
+        <div className="card">
+            <img className='cardimg' src="https://images.unsplash.com/photo-1677631231234-1234567890ab" alt="Card Image" />
+            <h2>I code</h2>
+            <p>i sucked coke of a lesbian in a church and gave her bbc</p>
+        </div>
+    );
+}
+
+export default Card
